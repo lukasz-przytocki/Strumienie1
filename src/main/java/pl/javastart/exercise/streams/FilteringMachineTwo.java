a1989bebea8ab113c6bb54cdbfe0b2c25ba58718
lukasz.przytocki <lukasz.przytocki@gmail.com>
2019-08-10 15:31:09 +0200

    Dodanie plików

--- src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
@@ -1,12 +1,16 @@
 package pl.javastart.exercise.streams;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilteringMachineTwo {
 
     // metoda powinna zwrócić imiona wszystkich niedorosłych osób (mniej niż 18 lat)
     public List<String> findKidNames(List<Person> ppl) {
-        return null;
+        return ppl.stream()
+                .filter(person -> person.getAge()<18)
+                .map(person -> person.getName())
+                .collect(Collectors.toList());
     }
 
     public List<User> convertPeopleToUsers(List<Person> people) {
